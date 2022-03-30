14d491c697df0a33215f21afa56ae06135ff9543
matti <mattis.hahn@student.reutlingen-university.de>
2022-03-30 20:50:36 +0200

    Von PC

--- Gerade.java
+++ Gerade.java
@@ -1,17 +1,17 @@
 public class Gerade {
-    Punkt p1;
-    Punkt p2;
+    private Punkt p1;
+    private Punkt p2;
 
-    Gerade(Punkt p1,Punkt p2){
+    public Gerade(Punkt p1,Punkt p2){
+        this.p1= p1;
+        this.p2= p2;
+    }
+    public void setPunkt1(Punkt p1) {
         this.p1=p1;
-        this.p2=p2;
-    }
-    public void setPunkt1(Punkt p) {
-        this.p1=p;
     }
 
-    public void setPunkt2(Punkt p) {
-        this.p2=p;
+    public void setPunkt2(Punkt p2) {
+        this.p2=p2;
     }
 
     public Punkt getPunkt1() {
@@ -21,23 +21,32 @@ public class Gerade {
     public Punkt getPunkt2() {
         return p2;
     }
-    public double Steigung(){ //m*x+b
-        double m= (getPunkt2().x - getPunkt1().x)/(getPunkt2().y - getPunkt1().y);
-        //double b1=getPunkt1().y-m*getPunkt1().x;
-        //double b2=getPunkt2().y-m*getPunkt2().x;
-        return m;
+    public Punkt berechneSchnittpunkt(Gerade g){
+
+        double m1 = (p2.getY() - p1.getY()) / (p2.getX() - p1.getX());
+        double c1 = p1.getY() - p1.getX() * m1;
+        Math.abs(m1);
+
+        double m2 = (g.p2.getY() - g.p1.getY()) / (g.p1.getX() - g.p2.getY());
+        Math.abs(m2);
+        double c2 = g.p1.getY() - g.p1.getX() * m2;
+
+        double s1 = (c2 - c1) / (m2 - m1); //vll m1-m2
+        double s2 = m1 * s1 + c1;
+
+        return new Punkt(s1,s2);
+
+    }
+
+    @Override
+    public String toString() {
+        return this.p1.toString() + "\n" + this.p2.toString();
     }
 
 
-    public Punkt berechneSchnittpunkt (Gerade g, double m){
 
 
-
-    }
-
-    public static void main(String[] args) {
-        System.out.println();
-
-    }
 
 }
+
+
--- Punkt.java
+++ Punkt.java
@@ -1,13 +1,11 @@
-import jdk.dynalink.linker.MethodTypeConversionStrategy;
 
 public class Punkt {
-    double x;
-    double y;
-
+    protected double x;
+    protected double y;
 
     Punkt(double x, double y){
         this.x=x;
-        this.y=x;
+        this.y=y;
     }
 
     public void setX(double x) {
@@ -27,25 +25,7 @@ public class Punkt {
 
         return y;
     }
-    void Punktausgabe(){
-        System.out.println("Punkt liegt bei x= "+ this.x + " y = " + this.y);
+    public String toString(){
+        return "x: " + x + ", y: " + y;
     }
-
-    public static void main(String[] args) {
-        Punkt p1= new Punkt(3,2);
-        //p1.Punktausgabe();
-        System.out.println(p1.getX());
-        System.out.println(p1.getY());
-        p1.setX(6);
-        p1.setY(33);
-        System.out.println(p1);
-        System.out.println(p1.toString());
-    }
-
-    @Override
-    public String toString() {
-        return "Koordinate X ist: "+ x +  " Koordinate Y ist: " + y;
-       // return "x: " + x + ", y: " + y;
-    }
-
 }
